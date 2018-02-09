0bf96620ad92bf3a44f35edbd891d88737545c8d
jared <jared.jacobson.2000@gmail.com>
2018-02-09 09:17:18 -0700

    set  up the base look of the navbar without any function in the buttons.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  userInfo: string;
+  loggedIn: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
