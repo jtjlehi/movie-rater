e98f0794b61f893b76a0f6962ba5b208d0f6c0b4
jared <jared.jacobson.2000@gmail.com>
2018-02-12 08:39:13 -0700

    imported all the firebaseModules into the firebase module

--- src/app/core/firebase/firebase.module.ts
+++ src/app/core/firebase/firebase.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule
+  ],
+  exports: [
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule
   ],
   declarations: []
 })
