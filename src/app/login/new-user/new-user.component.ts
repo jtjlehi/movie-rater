85766ef768f70e73e362543ea6436c47aead3b3f
jared <jared.jacobson.2000@gmail.com>
2018-02-14 08:50:11 -0700

    added the signin buttons to the new-user dialog

--- src/app/login/new-user/new-user.component.ts
+++ src/app/login/new-user/new-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../core/auth.service';
 
 @Component({
   selector: 'app-new-user',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewUserComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService) { }
 
   ngOnInit() {
+  }
+
+  signInWithGoogle() {
+    this.auth.googleLogin();
+  }
+
+  signInWithFacebook() {
+    this.auth.facebookLogin();
   }
 
 }
