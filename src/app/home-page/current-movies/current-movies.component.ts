832e51addb646e50f913c66ad6b4c4be6fc5ffad
jared <jared.jacobson.2000@gmail.com>
2018-02-16 10:30:17 -0700

    changed the current movies from hard coded in the template to movie comp

--- src/app/home-page/current-movies/current-movies.component.ts
+++ src/app/home-page/current-movies/current-movies.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {MovieService} from '../../Services/movies.service';
 import {Movie} from '../../Services/movies.interface';
 import { MovieObj } from '../movieObj.interface';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-current-movies',
@@ -17,7 +18,13 @@ export class CurrentMoviesComponent implements OnInit {
 
   ngOnInit() {
     this.movieService.getCurrentMovies().subscribe(movies => {
-      
+      console.log('current movies: ', movies);
+      // const returnMovie = _.chain(movies)
+      //   .map((movie) => {
+      //     title: movie
+      //   })
+      //   .value();
+      // console.log('changed movie: ', returnMovie);
     });
   }
 }
