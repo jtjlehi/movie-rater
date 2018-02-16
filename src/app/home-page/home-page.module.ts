5c62665848b8b604bf3c13e1e8d8c87b4335dcbf
jared <jared.jacobson.2000@gmail.com>
2018-02-16 08:15:51 -0700

    generated movie.component

--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -9,6 +9,7 @@ import { CurrentMoviesComponent } from './current-movies/current-movies.componen
 import {CoreModule} from '../core/core.module';
 import {MovieService} from '../Services/movies.service';
 import {HttpClientModule} from '@angular/common/http';
+import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
   imports: [
@@ -22,6 +23,7 @@ import {HttpClientModule} from '@angular/common/http';
     FriendsComponent,
     WishListComponent,
     UpcomingComponent,
+    MovieComponent,
   ],
   providers: [
     MovieService
