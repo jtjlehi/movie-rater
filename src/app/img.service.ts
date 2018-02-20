c4428181de2101217acfb84847b7d6d49c61092c
jared <jared.jacobson.2000@gmail.com>
2018-02-20 10:05:44 -0700

    added and implemented the getConfiguration method to the img service

--- src/app/img.service.ts
+++ src/app/img.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable';
+
+import { Configuration } from './Services/configuration.interface';
 
 @Injectable()
 export class ImgService {
 
-  constructor() { }
+  private imageUrl: string = 'https://api.themoviedb.org/3/configuration?api_key='
+  private apiKey: string = 'f1dc689823def4f561ce96b21153f793';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getConfiguration(): Observable<Configuration> {
+    return this.httpClient
+      .get<Configuration>(this.imageUrl + this.apiKey);
+  }
 
 }
