e73572030186d7b5858e8b51a7c3cfbb7adc6911
jared <jared.jacobson.2000@gmail.com>
2018-02-22 09:55:31 -0700

    defined the main methods for the WishlistService

--- src/app/home-page/services/wishlist.service.ts
+++ src/app/home-page/services/wishlist.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { MovieObj } from '../movieObj.interface';
 
 @Injectable()
 export class WishlistService {
 
+  userId: string;
+
   constructor() { }
+
+  // adds the item to firebase
+  public addMovie(movie: MovieObj) {
+    throw new Error('WishlistService.addMovie() not implemented');
+    // TODO: check if item already exists in firebase
+  }
+
+  public removeMovie(movieId) {
+    throw new Error('WishlistService.removeMovie() not implemented');
+  }
+
+  // displays the users wishlist
+  public displayWishList() {
+    throw new Error('WishlistService.displayWishList() not implemented');
+  }
 
 }
