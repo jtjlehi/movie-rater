ae627d112183722c35b8d4c22c94b997241118e8
jared <jared.jacobson.2000@gmail.com>
2018-02-26 10:21:56 -0700

    created the template for the add review service

--- src/app/home-page/services/reviews/reviews.service.ts
+++ src/app/home-page/services/reviews/reviews.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Review } from './reivew.interface';
 
 @Injectable()
 export class ReviewsService {
 
   constructor() { }
+
+  addReview(review: Review): void {
+    throw new Error('ReviewService.addReview() not implemented');
+  }
 
 }
