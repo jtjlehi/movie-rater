9b6d564e8ae310d6057c81a868de5cf845a896d9
jared <jared.jacobson.2000@gmail.com>
2018-02-26 10:25:20 -0700

    added angularFirestore and the AuthService to the review.service

--- src/app/home-page/services/reviews/reviews.service.ts
+++ src/app/home-page/services/reviews/reviews.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Review } from './review.interface';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { AuthService } from '../../../core/auth.service';
 
 @Injectable()
 export class ReviewsService {
 
-  constructor() { }
+  constructor(
+    afs: AngularFirestore,
+    auth: AuthService
+  ) { }
 
   addReview(review: Review): void {
     throw new Error('ReviewService.addReview() not implemented');
