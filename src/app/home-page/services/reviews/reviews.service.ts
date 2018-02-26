5587c6ce31b0f69883dc4d10375174154be166bf
jared <jared.jacobson.2000@gmail.com>
2018-02-26 10:34:06 -0700

    set the userDoc on the reviewService

--- src/app/home-page/services/reviews/reviews.service.ts
+++ src/app/home-page/services/reviews/reviews.service.ts
@@ -1,18 +1,43 @@
 import { Injectable } from '@angular/core';
 import { Review } from './review.interface';
-import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
+
 import { AuthService } from '../../../core/auth.service';
+import { User } from '../../../core/interfaces/user.interface';
 
 @Injectable()
 export class ReviewsService {
 
-  constructor(
-    afs: AngularFirestore,
-    auth: AuthService
-  ) { }
+  uid: string;
+  userDoc: AngularFirestoreDocument<User>;
 
-  addReview(review: Review): void {
+  constructor(
+    private afs: AngularFirestore,
+    private authService: AuthService
+  ) {
+    // subscribes to the users data as defined by the auth service
+    authService.userObservable.subscribe(user => {
+      if (!this.isUser) {
+        this.uid = user.uid;
+        this.setUserDoc();
+      }
+    });
+  }
+
+  public addReview(review: Review): void {
     throw new Error('ReviewService.addReview() not implemented');
+  }
+
+  // this sets the userDoc property
+  private setUserDoc() {
+    if (this.isUser) {
+      this.userDoc = this.afs.doc<User>(`users/${this.uid}`);
+    }
+  }
+
+  // checks if they are a user
+  private get isUser(): boolean {
+    return this.uid !== '';
   }
 
 }
