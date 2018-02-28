10503f7a07988e10ebc811adf06fef79f6d75a95
jared <jared.jacobson.2000@gmail.com>
2018-02-28 09:36:27 -0700

    created and implemented an addMovie() method to firebaseService in movie

--- src/app/home-page/movie/firebase.service.ts
+++ src/app/home-page/movie/firebase.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { MovieObj } from '../movieObj.interface';
 
 @Injectable()
 export class FirebaseService {
 
-  constructor() { }
+  constructor(
+    private afs: AngularFirestore
+  ) { }
+
+  public addMovie(movie: MovieObj) {
+    this.afs.collection<MovieObj>('movies').doc<MovieObj>(movie.fireId).set(movie);
+  }
 
 }
