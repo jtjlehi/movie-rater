1c09cb05d26b4f1bcdf3ed5345ceb9b4661af8c8
jared <jared.jacobson.2000@gmail.com>
2018-02-28 10:21:49 -0700

    added addLocalMovies and get neMovies() and subscribed to afs

--- src/app/home-page/movie/firebase.service.ts
+++ src/app/home-page/movie/firebase.service.ts
@@ -1,16 +1,32 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { MovieObj } from '../movieObj.interface';
+import * as _ from 'lodash';
 
 @Injectable()
 export class FirebaseService {
 
+  private localMovies: MovieObj[] = [];
+  private storedMovies: MovieObj[];
+
   constructor(
     private afs: AngularFirestore
-  ) { }
+  ) {
+    afs.collection<MovieObj>('movies').valueChanges().subscribe(storedMovies => {
+      this.storedMovies = storedMovies;
+    });
+  }
 
   public addMovie(movie: MovieObj) {
     this.afs.collection<MovieObj>('movies').doc<MovieObj>(movie.fireId).set(movie);
+  }
+
+  private get newMovies(): MovieObj[] {
+    return _.differenceBy(this.localMovies, this.storedMovies, 'fireId');
+  }
+
+  public addLocalMovies(addedMovies: MovieObj[]) {
+    this.localMovies = _.concat(this.localMovies, addedMovies);
   }
 
 }
