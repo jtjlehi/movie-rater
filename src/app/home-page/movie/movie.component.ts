461013c112972b7882c58f1ddcc7a7b23ca2947c
kmarriott <kurtis.s.marriott@gmail.com>
2018-03-02 09:21:26 -0700

    got functionality on user feedback for movie components based on whether or not they are logged in

--- src/app/home-page/movie/movie.component.ts
+++ src/app/home-page/movie/movie.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MovieObj } from '../movieObj.interface';
 import { WishlistService } from '../services/wishlist.service';
+import { MatSnackBar} from '@angular/material';
+import { AuthService} from '../../core/auth.service';
 
 @Component({
   selector: 'app-movie',
@@ -8,22 +10,29 @@ import { WishlistService } from '../services/wishlist.service';
   styleUrls: ['./movie.component.scss']
 })
 export class MovieComponent implements OnInit {
+  message: string = 'Added to your wishlist!';
+  done: string = 'Done';
   @Input() movie: MovieObj;
   @Input() inWishlist: boolean;
   @Input() width: string;
 
   constructor(
-    private wishlistService: WishlistService
+    private wishlistService: WishlistService,
+    private snackBar: MatSnackBar,
+    private authService: AuthService
   ) { }
 
   ngOnInit() {
   }
-
   addMovieToWishlist() {
     this.wishlistService.addMovie(this.movie);
   }
   removeMovieFromWishlist() {
     this.wishlistService.removeMovie(this.movie.fireId);
   }
-
+  openSnackBar(message: string, done: string) {
+      this.snackBar.open(message, done, {
+      duration: 2000
+    });
+  }
 }
