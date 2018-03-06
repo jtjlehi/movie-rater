b552fa33849e32cbb19f5071298f52bbd3782f13
jared <jared.jacobson.2000@gmail.com>
2018-03-06 09:32:05 -0700

    wrote the movie component so it actually tests if it is created

--- src/app/home-page/movie/movie.component.spec.ts
+++ src/app/home-page/movie/movie.component.spec.ts
@@ -1,25 +1,63 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MovieComponent } from './movie.component';
+import { WishlistService } from '../services/wishlist.service';
+import { MatSnackBar } from '@angular/material';
+import { AuthService } from '../../core/auth.service';
+import { CoreModule } from '../../core/core.module';
 
-// describe('MovieComponent', () => {
-//   let component: MovieComponent;
-//   let fixture: ComponentFixture<MovieComponent>;
+describe('MovieComponent', () => {
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ MovieComponent ]
-//     })
-//     .compileComponents();
-//   }));
+    let fixture: ComponentFixture<MovieComponent>;
+    let component: MovieComponent;
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(MovieComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    const wishlistServiceStub: Partial<WishlistService> = {
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    };
+
+    const authServiceStub: Partial<AuthService> = {
+
+    };
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [ MovieComponent ],
+            imports: [
+                CoreModule
+            ],
+            providers: [
+                {
+                    provide: WishlistService,
+                    useValue: wishlistServiceStub
+                },
+                {
+                    provide: AuthService,
+                    useValue: authServiceStub
+                }
+            ]
+        });
+        fixture = TestBed.createComponent(MovieComponent);
+        component = fixture.componentInstance;
+    });
+
+    it('should create', () => {
+        // TestBed.configureTestingModule({
+        //     declarations: [ MovieComponent ],
+        //     imports: [
+        //         CoreModule
+        //     ],
+        //     providers: [
+        //         {
+        //             provide: WishlistService,
+        //             useValue: wishlistServiceStub
+        //         },
+        //         {
+        //             provide: AuthService,
+        //             useValue: authServiceStub
+        //         }
+        //     ]
+        // });
+        // fixture = TestBed.createComponent(MovieComponent);
+        // component = fixture.componentInstance;
+        expect(component).toBeDefined();
+    });
+});
