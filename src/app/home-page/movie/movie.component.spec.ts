2fca0b4b0f010af71897a275257b1af2125458df
jared <jared.jacobson.2000@gmail.com>
2018-03-07 08:33:26 -0700

    wrote a movieObj

--- src/app/home-page/movie/movie.component.spec.ts
+++ src/app/home-page/movie/movie.component.spec.ts
@@ -5,11 +5,25 @@ import { WishlistService } from '../services/wishlist.service';
 import { MatSnackBar } from '@angular/material';
 import { AuthService } from '../../core/auth.service';
 import { CoreModule } from '../../core/core.module';
+import { MovieObj } from '../movieObj.interface';
 
 describe('MovieComponent', () => {
 
     let fixture: ComponentFixture<MovieComponent>;
     let component: MovieComponent;
+
+    const movieObj: MovieObj = {
+        title: 'gone with the wind',
+        description: 'an old movie that grossed more with inflation then any other movie',
+        image: {
+            url: 'someclassyurl.com',
+            poster_ref: 'adlsfja',
+            description: 'a picture of gone with the wind'
+        },
+        public_rating: 5,
+        app_rating: 5,
+        fireId: 'aoievn984adlkjf943'
+    };
 
     const wishlistServiceStub: Partial<WishlistService> = {
 
@@ -40,24 +54,6 @@ describe('MovieComponent', () => {
     });
 
     it('should create', () => {
-        // TestBed.configureTestingModule({
-        //     declarations: [ MovieComponent ],
-        //     imports: [
-        //         CoreModule
-        //     ],
-        //     providers: [
-        //         {
-        //             provide: WishlistService,
-        //             useValue: wishlistServiceStub
-        //         },
-        //         {
-        //             provide: AuthService,
-        //             useValue: authServiceStub
-        //         }
-        //     ]
-        // });
-        // fixture = TestBed.createComponent(MovieComponent);
-        // component = fixture.componentInstance;
         expect(component).toBeDefined();
     });
 });
