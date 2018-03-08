422a6bd3fdf76d7405cd81c1b651255cd673bc5a
bailiebutcher <bailiebutcher21@gmail.com>
2018-03-08 08:19:43 -0700

    Added Favicon.

--- src/app/core/material/material.module.ts
+++ src/app/core/material/material.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-// tslint:disable-next-line:max-line-length
 import { MatButtonModule, MatCheckboxModule, MatAutocompleteModule, MatDatepickerModule, MatFormFieldModule, MatInputModule, MatRadioModule, MatSelectModule, MatSliderModule, MatSlideToggleModule, MatMenuModule, MatSidenavModule, MatToolbarModule, MatListModule, MatGridListModule, MatCardModule, MatStepperModule, MatTabsModule, MatExpansionModule, MatButtonToggleModule, MatChipsModule, MatIconModule, MatProgressSpinnerModule, MatProgressBarModule, MatDialogModule, MatTooltipModule, MatSnackBarModule, MatTableModule, MatSortModule, MatPaginatorModule } from '@angular/material';
 
 @NgModule({
