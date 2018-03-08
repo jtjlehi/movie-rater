e27d06e5d1cb7e9cb00a9438a3996febc2ea95e8
jared <jared.jacobson.2000@gmail.com>
2018-03-08 08:19:35 -0700

    updated the prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyANO1QJx0fkI8XJBjmHB-JjKPJAlFO-Frg',
+    authDomain: 'movie-rater-app.firebaseapp.com',
+    databaseURL: 'https://movie-rater-app.firebaseio.com',
+    projectId: 'movie-rater-app',
+    storageBucket: 'movie-rater-app.appspot.com',
+    messagingSenderId: '342867243636'
+  }
 };
